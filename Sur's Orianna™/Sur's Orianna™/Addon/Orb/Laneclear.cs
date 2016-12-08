bbad2f5bd7b99e4905d2955acb52514a3498f33c
Surprise <drakateco@gmail.com>
2016-12-08 12:12:44 -0300

    6.24 | Sur's Orianna™ | Laneclear Fix

--- Sur's Orianna™/Sur's Orianna™/Addon/Orb/Laneclear.cs
+++ Sur's Orianna™/Sur's Orianna™/Addon/Orb/Laneclear.cs
@@ -7,58 +7,27 @@
     {
         public static void Get()
         {
-            if (MenuManager.mlane["method"].Cast<ComboBox>().CurrentValue == 0)
+            var m = EntityManager.MinionsAndMonsters.GetLaneMinions().OrderBy(o => o.Health).Where(c => c.IsValidTarget(SpellManager.Q.Range));
+            if (m != null)
             {
-                var m = EntityManager.MinionsAndMonsters.GetLaneMinions().OrderBy(o => o.Health).Where(c => c.IsValidTarget(SpellManager.Q.Range));
-                if (m != null)
+                if (MenuManager.mlane["q"].Cast<CheckBox>().CurrentValue)
                 {
-                    if (MenuManager.mlane["q"].Cast<CheckBox>().CurrentValue)
+                    if (SpellManager.Q.IsReady())
                     {
-                        if (SpellManager.Q.IsReady())
+                        var p = SpellManager.Q.GetBestCircularCastPosition(m);
+                        if (p.HitNumber >= MenuManager.mlane["minq"].Cast<Slider>().CurrentValue)
                         {
-                            var p = SpellManager.Q.GetBestCircularCastPosition(m);
-                            if (p.HitNumber >= MenuManager.mlane["minq"].Cast<Slider>().CurrentValue)
-                            {
-                                SpellManager.Q.Cast(p.CastPosition);
-                            }
-                        }
-                    }
-                    if (MenuManager.mlane["w"].Cast<CheckBox>().CurrentValue)
-                    {
-                        if (SpellManager.W.IsReady())
-                        {
-                            if (BallManager.WBall.CountEnemyMinionsNear >= MenuManager.mlane["minw"].Cast<Slider>().CurrentValue)
-                            {
-                                SpellManager.W.Cast();
-                            }
+                            SpellManager.Q.Cast(p.CastPosition);
                         }
                     }
                 }
-            }
-            else if (MenuManager.mlane["method"].Cast<ComboBox>().CurrentValue == 1)
-            {
-                var m = EntityManager.MinionsAndMonsters.GetLaneMinions().OrderByDescending(o => o.MaxHealth).Where(c => c.IsValidTarget(SpellManager.Q.Range));
-                if (m != null)
+                if (MenuManager.mlane["w"].Cast<CheckBox>().CurrentValue)
                 {
-                    if (MenuManager.mlane["q"].Cast<CheckBox>().CurrentValue)
+                    if (SpellManager.W.IsReady())
                     {
-                        if (SpellManager.Q.IsReady())
+                        if (BallManager.WBall.CountEnemyMinionsNear >= MenuManager.mlane["minw"].Cast<Slider>().CurrentValue)
                         {
-                            var p = SpellManager.Q.GetBestCircularCastPosition(m);
-                            if (p.HitNumber >= MenuManager.mlane["minq"].Cast<Slider>().CurrentValue)
-                            {
-                                SpellManager.Q.Cast(p.CastPosition);
-                            }
-                        }
-                    }
-                    if (MenuManager.mlane["w"].Cast<CheckBox>().CurrentValue)
-                    {
-                        if (SpellManager.W.IsReady())
-                        {
-                            if (BallManager.WBall.CountEnemyMinionsNear >= MenuManager.mlane["minw"].Cast<Slider>().CurrentValue)
-                            {
-                                SpellManager.W.Cast();
-                            }
+                            SpellManager.W.Cast();
                         }
                     }
                 }
