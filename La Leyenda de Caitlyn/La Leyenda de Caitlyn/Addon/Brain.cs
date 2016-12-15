08c0056fbe1750e95c3109d932fd9b3d06851426
Surprise <drakateco@gmail.com>
2016-12-14 23:28:43 -0300

    15/12/2016 | La Leyenda de Caitlyn | SBTW no menu, no drawings.

---
+++ La Leyenda de Caitlyn/La Leyenda de Caitlyn/Addon/Brain.cs
@@ -0,0 +1,83 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+
+namespace La_Leyenda_de_Caitlyn.Addon
+{
+    public static class Brain
+    {
+        private static AIHeroClient Caitlyn => Player.Instance;
+
+        public static float RealAARange = 715;
+
+        public static int Time(this Spell.Skillshot s, Obj_AI_Base endpos)
+        {
+            return (int)((Caitlyn.Distance(endpos.ServerPosition) / s.Speed) * 1000) + s.CastDelay;
+        }
+
+        public static int RTime(Obj_AI_Base endpos)
+        {
+            return (int)((Caitlyn.Distance(endpos.ServerPosition) / R.Speed) * 1000) + R.CastDelay;
+        }
+
+        public static bool IsInMinimumRange(this Obj_AI_Base obj, float min, float max)
+        {
+            return !obj.IsInRange(Player.Instance, min) && obj.IsInRange(Player.Instance, max);
+        }
+
+        public static bool IsCCd(this Obj_AI_Base obj)
+        {
+            if (obj.HasBuffOfType(BuffType.Charm) || obj.HasBuffOfType(BuffType.Fear)
+                || obj.HasBuffOfType(BuffType.Stun) || obj.HasBuffOfType(BuffType.Knockup)
+                || obj.HasBuffOfType(BuffType.Knockback) || obj.HasBuffOfType(BuffType.Snare)
+                || obj.HasBuffOfType(BuffType.Taunt) || obj.HasBuffOfType(BuffType.Suppression)
+                || obj.IsRooted || obj.IsStunned)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static class W
+        {
+            public const int CastDelay = 1100;
+
+            public const int Speed = int.MaxValue;
+
+            public const int Width = 67;
+        }
+
+        public static class R
+        {
+            public static float GetRange()
+            {
+                var i = 0;
+
+                switch (SpellHandler.R.Level)
+                {
+                    case 1:
+                        i = 2000;
+                        break;
+
+                    case 2:
+                        i = 2500;
+                        break;
+
+                    case 3:
+                        i = 3000;
+                        break;
+                }
+
+                return i;
+            }
+
+            public const int CastDelay = 1000;
+
+            public const int Speed = 3200;
+
+            public const int Width = 65;
+        }
+    }
+}
---
+++ La Leyenda de Caitlyn/La Leyenda de Caitlyn/Addon/Orb/Laneclear.cs
@@ -0,0 +1,29 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using System.Linq;
+
+namespace La_Leyenda_de_Caitlyn.Addon.Orb
+{
+    public static class Laneclear
+    {
+        private static AIHeroClient Caitlyn => Player.Instance;
+
+        public static void Get()
+        {
+            if (Caitlyn.IsDead || Caitlyn.HasBuff("recall")) return;
+
+            if (SpellHandler.Q.IsReady() && Caitlyn.ManaPercent > 59)
+            {
+                var minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Caitlyn.Position).Where(x => x.IsInRange(Caitlyn, SpellHandler.Q.Range));
+                if (minions != null)
+                {
+                    var p = SpellHandler.Q.GetBestLinearCastPosition(minions);
+                    if (p.HitNumber > 2)
+                    {
+                        SpellHandler.Q.Cast(p.CastPosition);
+                    }
+                }
+            }
+        }
+    }
+}
