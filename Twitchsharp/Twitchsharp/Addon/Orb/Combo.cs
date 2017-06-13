48ab9ea16e759df5d9bda33c14676c90e1e13e07
ElDivines <scugnizzo.napulitan@hotmail.it>
2017-06-14 00:38:53 +0200

    Combo.cs
    Just added QBack safe recall logic, you can easly edit and put it where u want, I don't know how do it ^^

--- Twitchsharp/Twitchsharp/Addon/Orb/Combo.cs
+++ Twitchsharp/Twitchsharp/Addon/Orb/Combo.cs
@@ -50,6 +50,58 @@ namespace Twitchsharp.Addon.Orb
                     {
                         SpellHandler.E.Cast();
                     }
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Menu.Values;
+using System.Linq;
+
+namespace Twitchsharp.Addon.Orb
+{
+    public static class Combo
+    {
+        private static AIHeroClient Twitch => Player.Instance;
+
+        public static void Get()
+        {
+            if (MenuHandler.combo["q"].Cast<CheckBox>().CurrentValue == true)
+            {
+                var enemies = EntityManager.Heroes.Enemies.Where(x => x.IsValid && !x.IsDead).FirstOrDefault(d => d.IsInRange(Twitch, 1200));
+                if (enemies != null)
+                {
+                    if (enemies.IsInMinimumRange(Brain.BaseAA, Brain.ExtendedAA) && SpellHandler.Q.IsReady())
+                    {
+                        SpellHandler.Q.Cast();
+                    }
+                }
+            }
+
+            if (MenuHandler.combo["w"].Cast<CheckBox>().CurrentValue == true)
+            {
+                var t = TargetSelector.GetTarget(SpellHandler.W.Range, DamageType.True);
+                if (t != null && !t.IsInvulnerable)
+                {
+                    var p = SpellHandler.W.GetPrediction(t);
+                    if (p != null && p.HitChance > HitChance.Medium)
+                    {
+                        if (t.IsInMinimumRange(Brain.BaseAA, SpellHandler.W.Range) && SpellHandler.W.IsReady())
+                        {
+                            SpellHandler.W.Cast(t);
+                        }
+                    }
+                }
+            }
+
+            if (MenuHandler.combo["e"].Cast<CheckBox>().CurrentValue == true)
+            {
+                var enemies = EntityManager.Heroes.Enemies.Where(x => x.IsValid && !x.IsInvulnerable && !x.IsDead && x.IsVenom()).FirstOrDefault(d => d.IsInRange(Twitch, SpellHandler.E.Range));
+                if (enemies != null)
+                {
+                    var hp = Prediction.Health.GetPrediction(enemies, 275);
+                    if (DamageHandler.SD(enemies) >= hp && SpellHandler.E.IsReady())
+                    {
+                        SpellHandler.E.Cast();
+                    }
                 }
             }
 
@@ -59,7 +111,7 @@ namespace Twitchsharp.Addon.Orb
                 if (t != null)
                 {
                     //Alone Logic
-                    if (t.CountAlliesInRange(Brain.BaseAA) == 0)
+                    if (t.CountAllyChampionsInRange(Brain.BaseAA) == 0)
                     {
                         if (t.IsInMinimumRange(Brain.BaseAA, Brain.ExtendedAA))
                         {
@@ -89,8 +141,8 @@ namespace Twitchsharp.Addon.Orb
                     //TF Logic
                     if (MenuHandler.combo["tf"].Cast<CheckBox>().CurrentValue)
                     {
-                        if (Twitch.CountAlliesInRange(1000) > t.CountEnemiesInRange(1500)
-                            && t.CountAlliesInRange(500) > 0)
+                        if (Twitch.CountAllyChampionsInRange(1000) > t.CountEnemyChampionsInRange(1500)
+                            && t.CountAllyChampionsInRange(500) > 0)
                         {
                             if (t.IsInMinimumRange(Brain.BaseAA, Brain.ExtendedAA))
                             {
@@ -100,9 +152,9 @@ namespace Twitchsharp.Addon.Orb
                     }
                     else
                     {
-                        if (t.CountAlliesInRange(500) > 0
-                            && Twitch.CountAlliesInRange(1000) > 1
-                            && Twitch.CountEnemiesInRange(1500) > 1)
+                        if (t.CountAllyChampionsInRange(500) > 0
+                            && Twitch.CountAllyChampionsInRange(1000) > 1
+                            && Twitch.CountAllyChampionsInRange(1500) > 1)
                         {
                             if (t.IsInMinimumRange(Brain.BaseAA, Brain.ExtendedAA))
                             {
@@ -112,6 +164,14 @@ namespace Twitchsharp.Addon.Orb
                     }
                 }
             }
+        }
+        //QBack Safe invisible logic
+        private static void auto_baseQ(Spellbook sender, SpellbookCastSpellEventArgs eventArgs)
+            {
+            if (eventArgs.Slot != SpellSlot.Recall || SpellHandler.Q.IsReady() || MenuHandler.misc["QRecall"].Cast<CheckBox>().CurrentValue || MenuHandler.misc["QRecall2"].Cast<KeyBind>().CurrentValue) return;
+                        SpellHandler.Q.Cast();
+                        Core.DelayAction(() => ObjectManager.Player.Spellbook.CastSpell(SpellSlot.Recall), SpellHandler.Q.CastDelay + 300);
+                        eventArgs.Process = false;
         }
     }
 }
