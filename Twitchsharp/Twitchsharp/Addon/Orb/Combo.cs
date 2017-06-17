8828e8a9049fe91540144e2a250447601a5f58cc
Surprise <drakateco@gmail.com>
2017-06-17 07:31:52 -0400

    Fixing Pull Request #2

--- Twitchsharp/Twitchsharp/Addon/Orb/Combo.cs
+++ Twitchsharp/Twitchsharp/Addon/Orb/Combo.cs
@@ -1,55 +1,3 @@
-﻿using EloBuddy;
-using EloBuddy.SDK;
-using EloBuddy.SDK.Enumerations;
-using EloBuddy.SDK.Menu.Values;
-using System.Linq;
-
-namespace Twitchsharp.Addon.Orb
-{
-    public static class Combo
-    {
-        private static AIHeroClient Twitch => Player.Instance;
-
-        public static void Get()
-        {
-            if (MenuHandler.combo["q"].Cast<CheckBox>().CurrentValue == true)
-            {
-                var enemies = EntityManager.Heroes.Enemies.Where(x => x.IsValid && !x.IsDead).FirstOrDefault(d => d.IsInRange(Twitch, 1200));
-                if (enemies != null)
-                {
-                    if (enemies.IsInMinimumRange(Brain.BaseAA, Brain.ExtendedAA) && SpellHandler.Q.IsReady())
-                    {
-                        SpellHandler.Q.Cast();
-                    }
-                }
-            }
-
-            if (MenuHandler.combo["w"].Cast<CheckBox>().CurrentValue == true)
-            {
-                var t = TargetSelector.GetTarget(SpellHandler.W.Range, DamageType.True);
-                if (t != null && !t.IsInvulnerable)
-                {
-                    var p = SpellHandler.W.GetPrediction(t);
-                    if (p != null && p.HitChance > HitChance.Medium)
-                    {
-                        if (t.IsInMinimumRange(Brain.BaseAA, SpellHandler.W.Range) && SpellHandler.W.IsReady())
-                        {
-                            SpellHandler.W.Cast(t);
-                        }
-                    }
-                }
-            }
-
-            if (MenuHandler.combo["e"].Cast<CheckBox>().CurrentValue == true)
-            {
-                var enemies = EntityManager.Heroes.Enemies.Where(x => x.IsValid && !x.IsInvulnerable && !x.IsDead && x.IsVenom()).FirstOrDefault(d => d.IsInRange(Twitch, SpellHandler.E.Range));
-                if (enemies != null)
-                {
-                    var hp = Prediction.Health.GetPrediction(enemies, 275);
-                    if (DamageHandler.SD(enemies) >= hp && SpellHandler.E.IsReady())
-                    {
-                        SpellHandler.E.Cast();
-                    }
 using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Enumerations;
@@ -64,7 +12,7 @@ namespace Twitchsharp.Addon.Orb
 
         public static void Get()
         {
-            if (MenuHandler.combo["q"].Cast<CheckBox>().CurrentValue == true)
+            if (MenuHandler.combo["q"].Cast<CheckBox>().CurrentValue)
             {
                 var enemies = EntityManager.Heroes.Enemies.Where(x => x.IsValid && !x.IsDead).FirstOrDefault(d => d.IsInRange(Twitch, 1200));
                 if (enemies != null)
@@ -76,7 +24,7 @@ namespace Twitchsharp.Addon.Orb
                 }
             }
 
-            if (MenuHandler.combo["w"].Cast<CheckBox>().CurrentValue == true)
+            if (MenuHandler.combo["w"].Cast<CheckBox>().CurrentValue)
             {
                 var t = TargetSelector.GetTarget(SpellHandler.W.Range, DamageType.True);
                 if (t != null && !t.IsInvulnerable)
@@ -92,7 +40,7 @@ namespace Twitchsharp.Addon.Orb
                 }
             }
 
-            if (MenuHandler.combo["e"].Cast<CheckBox>().CurrentValue == true)
+            if (MenuHandler.combo["e"].Cast<CheckBox>().CurrentValue)
             {
                 var enemies = EntityManager.Heroes.Enemies.Where(x => x.IsValid && !x.IsInvulnerable && !x.IsDead && x.IsVenom()).FirstOrDefault(d => d.IsInRange(Twitch, SpellHandler.E.Range));
                 if (enemies != null)
@@ -105,7 +53,7 @@ namespace Twitchsharp.Addon.Orb
                 }
             }
 
-            if (MenuHandler.combo["r"].Cast<CheckBox>().CurrentValue == true)
+            if (MenuHandler.combo["r"].Cast<CheckBox>().CurrentValue)
             {
                 var t = TargetSelector.GetTarget(Brain.ExtendedAA, DamageType.Physical);
                 if (t != null)
@@ -164,14 +112,6 @@ namespace Twitchsharp.Addon.Orb
                     }
                 }
             }
-        }
-        //QBack Safe invisible logic
-        private static void auto_baseQ(Spellbook sender, SpellbookCastSpellEventArgs eventArgs)
-            {
-            if (eventArgs.Slot != SpellSlot.Recall || SpellHandler.Q.IsReady() || MenuHandler.misc["QRecall"].Cast<CheckBox>().CurrentValue || MenuHandler.misc["QRecall2"].Cast<KeyBind>().CurrentValue) return;
-                        SpellHandler.Q.Cast();
-                        Core.DelayAction(() => ObjectManager.Player.Spellbook.CastSpell(SpellSlot.Recall), SpellHandler.Q.CastDelay + 300);
-                        eventArgs.Process = false;
         }
     }
 }
